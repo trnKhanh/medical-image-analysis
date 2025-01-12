081b5142e45827ebdbed61623e56e5b2d12d03b0
trnKhanh <tnk29082003@gmail.com>
2025-01-12 12:30:35 +0700

    Add spatial transform

--- src/training/unet_trainer.py
+++ src/training/unet_trainer.py
@@ -286,21 +286,33 @@ class UNetTrainer(BaseTrainer):
             return None
 
         transforms = []
-        # transforms.append(
-        #     RandomTransform(RandomGaussianNoise(sigma=(0, 0.1)), p=0.1)
-        # )
-        # transforms.append(
-        #     RandomTransform(RandomGaussianBlur(sigma=(0.5, 1)), p=0.2)
-        # )
+        transforms.append(
+            RandomTransform(
+                RandomAffine(scale=(0.7, 1.4)),
+                p=0.2
+            )
+        )
+        transforms.append(
+            RandomTransform(
+                RandomAffine(degrees=(-15, 15)),
+                p=0.2
+            )
+        )
+        transforms.append(
+            RandomTransform(RandomGaussianNoise(sigma=(0, 0.1)), p=0.1)
+        )
+        transforms.append(
+            RandomTransform(RandomGaussianBlur(sigma=(0.5, 1)), p=0.2)
+        )
         transforms.append(
             RandomTransform(RandomBrightness(brightness=0.25), p=0.15)
         )
         transforms.append(
             RandomTransform(RandomContrast(contrast=0.25), p=0.15)
         )
-        # transforms.append(
-        #     RandomTransform(SimulateLowRes(scale=(0.5, 1)), p=0.15)
-        # )
+        transforms.append(
+            RandomTransform(SimulateLowRes(scale=(0.5, 1)), p=0.15)
+        )
         transforms.append(RandomTransform(RandomGamma(gamma=(0.7, 1.5)), p=0.1))
 
         return ComposeTransform(transforms)
